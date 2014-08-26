f0e892df209d53b8149f869b1eacdf47f393138f
Mary-Jane Smyth <maryjane.smyth@abiquo.com>
2014-08-26 10:44:19 +0200

    Process event viewing privileges

---
+++ process_events_privileges.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python3 -tt
+
+import sys
+import re
+import os
+
+# create a set of tables of events and actions 
+# Input file should be platform/model/event-model-transport/src/main/java/com/abiquo/event/model/enumerations/EntityAction.java
+# Output file should be output_files/wiki_events.txt
+# Note that I have hacked this program to make it leave out the java muck surrounding the enumerations!
+# If there are any problems: it uses RS = Master, and checks for * in records, and doesn't print headers or details of the first record and last two records! 
+# NOTE that this script produces two other output files - entity_action_list.txt, which is an input to the tracer processing scripts
+# and entity_list.txt which is a list of all entities as a starting point for the events and tracer tables for users... 
+# actually now I think about it, the user events have "USER" maybe this isn't necessary... aagh
+# ./process_events.sh ../platform/model/event-model-transport/src/main/java/com/abiquo/event/model/enumerations/EntityAction.java output_files/wiki_events.txt
+#
+# Note that the following comments are out of date:
+# Convert EventType.java to Confluence Wiki Format
+# Note that it is best to cut the java code from the beginning and end of the file as that is not included in the scope of this script
+# Read EventType.java file $1 and write to wiki output $2
+# Mark comments starting with // as header records
+# Strip whitespace in the file
+# Set the record separator as "), "
+# Print the first field of lines with one field beginning with // as h5. in wiki format after stripping non-numerics, then print header table row with first letter only in caps
+# If header record, print header 
+# Note that the last record is not processed properly because of EOF or something
+# Print normal error message lines separated by | for wiki format
+## replace {} with ""
+## replace [] with ()
+# Leaves other // comments in file - check for these
+
+def main():
+	FS="new KEYS"
+	RS="public static final class|public static final Action|Master"
+	KeyTotal = " "
+	NRSave = 0
+	EntityName = ""
+	ActionName = ""
+	startout = {}
+	entityout = {}
+	endout = {}
+	entityheader = {}
+	entity_space_action = {}
+	entity_for_users = {}
+	KeyInfo = {}
+	KeySplit = {}
+	keyHappy = " "
+	entity_action_list_git_dir = "../platform/model/event-model-transport/src/main/java/com/abiquo/event/model/enumerations"
+	entity_action_git_file = "EntityAction.java"
+
+	entity_list = "input_files/entity_list.txt"
+	with open (os.path.join(entity_action_list_git_dir,entity_action_git_file), "r") as eafile:
+		eadata=eafile.read().replace('\n', '')
+
+	ea_records = re.split(RS,eadata)
+
+	for NR,ear in enumerate(ea_records):
+		print ("NR: ", NR)
+		NRSave = NR
+		ea_fields = re.split(FS,ear)
+		if re.match ("\*",ear):
+			print ("ear: ",ear)
+			break
+
+		if len(ea_fields) > 1:
+			EntityAction = ea_fields[0].split("=")
+			EntityActionNames = EntityAction[0].split(" ")
+			EntityName = EntityActionNames[0]
+
+			EntityName = re.sub("\<","",EntityName)
+			EntityName = re.sub("\>","",EntityName)
+
+			ActionName = EntityActionNames[1]
+			entity_space_action[NR] = EntityName + " " + ActionName
+	
+			print ("EntityName: ", EntityName)
+			print ("ActionName: ", ActionName)
+
+			mykeysstring = " ".join((ea_fields))
+			print ("mykeysstring: ",mykeysstring)
+
+			thekeys = re.findall('(?<=KEYS\.)[A-Z_]*',mykeysstring)
+			if thekeys:
+				print(thekeys)
+
+
+
+			# for MyKey in range(1,4):
+			# 	print ("ea_fieldsMK: ",ea_fields[MyKey])
+			# 	if re.match ("KEYS",ea_fields[MyKey]):
+			# 		KeyInput = ea_fields[MyKey]
+			# 		KeyInput = re.sub("KEYS.","",KeyInput)
+			# 		KeyInput = re.sub("\\[","",KeyInput)
+			# 		KeyInput = re.sub("\]","",KeyInput)
+			# 		KeyInput = re.sub("\{","",KeyInput)
+			# 		KeyInput = re.sub("\}","",KeyInput)
+			# 		KeyInput = re.sub("\);","",KeyInput)
+			# 		KeyInput = re.sub("\n","",KeyInput)
+			# 		KeyInput = re.sub("\*","",KeyInput)
+			# 		KeyInfo[MyKey] = copy(KeyInput)
+			# 	else:
+			# 		KeyInfo[MyKey] = " "
+			# print ("KeyInfo: ", str(KeyInfo))
+			# for mk in KeyInfo:	
+			# 	KeyTotal = KeyTotal + KeyInfo[mk] 
+			# print ("KeyTotal: ",KeyTotal)	
+			# KeySplit = KeyTotal.split ("\,|[ \t\n]+")
+			# keyHappy = " ";
+			# for KeyItem in KeySplit:
+			# 	if re.match("/[A-Z][a-z]/",KeyItem):
+			# 		if re.match("/[A-Z][a-z]/",keyHappy):
+			# 			keyHappy = keyHappy + ", " + KeyItem
+			# 		else:
+			# 			keyHappy = copy(KeyItem)
+			
+			startout[NR] = "| " 
+			print ("ActionName: ", ActionName)
+			endout[NR] =  " | " + ActionName + " | " + keyHappy + " |" 
+	 
+		if len(ea_fields) == 1:
+			eIndex = NR + 1
+			ENameHeading = ea_fields[0]
+			ENameHeading = re.sub("\{","",ENameHeading)
+			ENameHeading = re.sub ("\n","",ENameHeading)
+			entityout[eIndex] = ENameHeading
+			entity_for_users[NR] = ENameHeading
+			entityheader_fixcase = ENameHeading
+			entityheader_fixcase = re.sub ("_"," ",entityheader_fixcase)
+			entityheader_fixcase = entityheader_fixcase.lower()
+			capital_letter = entityheader_fixcase[:2]
+			capital_letter = capital_letter.upper()
+			rest_of_letters = entityheader_fixcase[2:]
+			entityheader_for_sub = capital_letter + rest_of_letters
+			entityheader_for_sub = re.sub("ip","IP",entityheader_for_sub)
+			entityheader_for_sub = re.sub ("Ssl","SSL",entityheader_for_sub)
+			entityheader_for_sub = re.sub ("ldap","LDAP",entityheader_for_sub)
+			entityheader[NR] = entityheader_for_sub
+	 		
+
+	print ("|| Entity || Action || Additional Information ||")
+	NRI = NRSave - 3
+	for s in range(1, NRI): 
+
+		if s in endout:
+			if re.match ("/[A-Z][a-z]/",endout[s]):
+				print (startout[s], entityout[s], endout[s])
+		else:
+			if s in entityheader:
+				print ("|| h6. ", entityheader[s], " || || ||")
+
+  
+# Calls the main() function
+if __name__ == '__main__':
+	main()
---
+++ process_events_tracers_privileges.py
@@ -0,0 +1,225 @@
+#!/usr/bin/python3 -tt
+
+import sys
+import re
+import os
+
+def main():
+	entity_actions_file = []
+	entity_names = {}
+	entity_actions = {}
+	entity_compounds = []
+
+	user_entities = []
+	tracer_keys = []
+	tracer_texts = []
+
+	tracer_keys_matched = []
+	tracer_keys_subbed = []
+
+
+	tracer_messages = []
+	tracers = []
+
+	indexed_messages = {}
+	tkey_errors = {}
+	tkey_severities = {}
+	eaindex = 0
+	tmindex = 0
+	tindex = 0
+	extra_text = {}
+	extra_bit = {}
+	outputline = {}
+	group_key_change =  {}
+	groupkey = {}
+	previous_key = " "
+	out_subdir = "output_files"
+	wiki_event_tracer_privileges_file = "wiki_event_tracer_privileges.txt"
+	git_dir = "../platform/m/src/main/resources/messages"
+	git_events_security_dir = "../platform/api/src/main/resources/events"
+	events_security_privileges_file = "events-security.properties"
+	input_subdir = "input_files"
+	entity_file = "entity_list.txt"
+	prop_file = "tracer.properties"
+	entity_action_file = "entity_action_list.txt"
+	entity_actions_file = [ea.strip() for ea in open(os.path.join(input_subdir,entity_action_file))]
+	events_security_file = [es.strip() for es in open(os.path.join(git_events_security_dir,events_security_privileges_file))]
+	header = "|| Entity || Action || Severity || Tracer || API Error || Privileges ||\n"
+
+	entities = [ue.strip() for ue in open(os.path.join(input_subdir,entity_file))]
+  
+	tracer_messages = [tm.strip() for tm in open(os.path.join(git_dir,prop_file))]
+	tracer_messages.sort()
+	event_list_with_privileges = {}
+
+#	for ue in user_entities:
+#		print("User entity: ",ue)
+    	
+	for eai in entity_actions_file:
+		print ("eai: ",eai)
+		eaSplit = eai.split(" ")
+		print ("eaSplit: ",eaSplit)
+		entity_compound = eaSplit[0] + "_" + eaSplit[1]
+		entity_compound_dot = eaSplit[0] + "." + eaSplit[1]
+		entity_compounds.append(entity_compound)
+		entity_names[entity_compound] = eaSplit[0].strip()
+		entity_actions[entity_compound] = eaSplit[1]
+	
+	for esi in events_security_file:
+		print ("esi: ",esi)
+		esSplit = esi.split("=")
+		es_privilege = esSplit[0]
+		es_privilege = es_privilege.strip()
+		es_event_list = esSplit[1]
+		elSplit = es_event_list.split(",")
+		for els in elSplit:		
+#			substitute the dot for an underscore
+			event_list_event = re.sub("\.","_",els)
+			event_list_event = event_list_event.strip()
+			if event_list_event not in event_list_with_privileges:
+				event_list_with_privileges[event_list_event] = [es_privilege]
+			else:
+				if es_privilege not in event_list_with_privileges[event_list_event]:
+					event_list_with_privileges[event_list_event].append(es_privilege)
+
+		
+	for tmi in tracer_messages:
+		if not re.search("^\#",tmi):
+			if re.search("=",tmi):
+				tracerSplit = tmi.split("=")
+				tracer_keys.append((tracerSplit[0]).strip())
+				tracer_text = tracerSplit[1].strip()
+				tracer_text = re.sub("\{","",tracer_text)
+				tracer_text = re.sub("\}","",tracer_text)
+				tracer_text = re.sub("details\.","",tracer_text)
+				tracer_text = re.sub("entity\.","",tracer_text)
+				tracer_text = re.sub("\\\\:",":",tracer_text)
+				tracer_text = re.sub("details","",tracer_text)
+				tracer_text = re.sub("\[","(",tracer_text)
+				tracer_text = re.sub("\]",")",tracer_text)
+#				tracer_text = re.sub("\'\.","\'",tracer_text)
+#     gsub(/{/,"",n); gsub(/}/,"",n); gsub(/\[/,"(",n); gsub(/\]/,")",n); gsub(/(\\)(n)/,"- ",n); gsub(/(\\)(:)/,":",n); gsub("details.","",n); gsub("entity.","",n);
+				tracer_texts.append(tracer_text)
+				midex = tracerSplit[0].strip()
+#				indexed_messages[midex]=tracer_text
+				indexed_messages[midex]=tracer_text
+
+	entity_compounds.sort()
+	entity_compounds.sort(key=len, reverse=True)	
+
+	tracer_keys.sort()
+	tracer_keys.sort(key=len, reverse=True)
+
+	for eck,eci in enumerate(entity_compounds):
+		print ("eci: ",eci)
+		ecix = "^" + eci
+		for tki,tkey in enumerate(tracer_keys):
+			if re.search(ecix,tkey):
+				print ("tracer_keys: ",tkey)
+				tracer_keys_matched.append(tkey)
+
+				tkey_sub = re.sub(ecix,"",tkey)
+				tkey_sub = re.sub("^_","",tkey_sub)
+				tkey_sub = re.sub("_$","",tkey_sub)
+
+				print ("tracer_keys_substitution: ",tkey_sub)
+				extra_text[tkey] = " "
+				extra_bit[tkey] = " "
+				tkey_severities[tkey] = " " 
+				tkey_errors[tkey] = " "
+				info_split = [" "," "]
+
+				if re.search("INFO",tkey_sub):
+					info_split = tkey_sub.split("INFO")
+					tkey_severities[tkey] = " (i) "
+					if re.search("[A-Z]+",info_split[0]):
+						extra_texts = info_split[0]
+						extra_text[tkey] = re.sub("_$","",extra_texts)
+					if re.search("[A-Z]+",info_split[1]):	
+						extra_bits = info_split[1]
+#						extra_bit[tkey] = re.sub("_$","",extra_bits)
+						extra_bits = re.sub("_$","",extra_bits)											
+						extra_bits = re.sub("^_","",extra_bits)
+						tkey_errors[tkey] = extra_bits
+
+				if re.search("WARN",tkey_sub):
+					info_split = tkey_sub.split("WARN")
+					tkey_severities[tkey] = " (!) "
+					if re.search("[A-Z]+",info_split[0]):
+						extra_texts = info_split[0]
+						extra_text[tkey] = re.sub("_$","",extra_texts)				
+					if re.search("[A-Z]+",info_split[1]):	
+						extra_bits = info_split[1]
+						extra_bits = re.sub("_$","",extra_bits)
+						extra_bits = re.sub("^_","",extra_bits)						
+						tkey_errors[tkey] = extra_bits
+
+
+				if re.search("ERROR",tkey_sub):
+					info_split = tkey_sub.split("ERROR")
+					tkey_severities[tkey] = " (-) "	
+					tkey_error = info_split[1]
+					tkey_error = re.sub("^_","",tkey_error)
+					tkey_error = re.sub("_$","",tkey_error)
+					tkey_errors[tkey] = tkey_error	
+					extra_texts = info_split[0]
+					extra_text[tkey] = re.sub("_$","",extra_texts)				
+					extra_bit[tkey] = " "
+
+				tkey_sub = re.sub("^_","",tkey_sub)
+				tkey_sub = re.sub("_$","",tkey_sub)
+
+				tracer_keys_subbed.append(tkey_sub)
+#				print ("eci: ",eci," \t | tkey: ",tkey," \t |",tki) 
+#				print ("eneci: ***",entity_names[eci],"*** \t" )
+#				print ("tkey: ****",tkey,"****")
+#				outputline[tkey] = "|  | " + entity_actions[eci] + " " + extra_text[tkey] + " | " + tkey_severities[tkey] + " | " + indexed_messages[tkey] + " | "  + tkey_errors[tkey]  + " | \n"						
+#				print("entity_names.eci: *****",entity_names[eci],"*****")					
+#				print("entity_actions.eci: *****",entity_actions[eci],"*****")
+#				print("extra_text.tkey: *****",extra_text[tkey],"*****")					
+#				print("tkey_severities.tkey: *****",tkey_severities[tkey],"*****")					
+#				print("indexed_messages.tkey: *****",indexed_messages[tkey],"*****")					
+#				print("tkey_errors.tkey: *****",tkey_errors[tkey],"*****")
+				event_list_with_privileges_print = ""
+				for elwp in event_list_with_privileges[eci]:
+					elwp_item = elwp.strip('\'')
+					if not event_list_with_privileges_print:
+						event_list_with_privileges_print =  elwp_item
+					else:
+						event_list_with_privileges_print = event_list_with_privileges_print + ", " + elwp_item
+						
+				outputline[tkey] = "| " + entity_names[eci] + " | " + entity_actions[eci] + " " + extra_text[tkey] + " | " + tkey_severities[tkey] + " | " + indexed_messages[tkey] + " | "  + tkey_errors[tkey]  + " | " + event_list_with_privileges_print  + " | \n"
+#				print ("tkey: ***",tkey.strip(),"***")
+#				else:
+#					outputline[tkey] = "|  | " + entity_actions[eci] + " " + extra_text[tkey] + " | " + tkey_severities[tkey] + " | " + indexed_messages[tkey] + " | "  + tkey_errors[tkey]  + " | \n"						
+#				outputline[tkey] = "| " + entity_names[eci] + " | " + entity_actions[eci] + " " + extra_text[tkey] + " " + extra_bit[tkey] + " | " + tkey_severities[tkey] + " | " + indexed_messages[tkey] + " | "  + tkey_errors[tkey]  + " | \n"
+#				outputline[tkey] = "| " + entity_names[eci] + " | " + entity_actions[eci] + " " + extra_text[tkey] + " " + extra_bit[tkey] + " | " + tkey_severities[tkey] + " | " + tracer_texts[tki] + " | "  + tkey_errors[tkey]  + " | \n"
+				groupkey[tkey] = eci
+
+	with open(os.path.join(out_subdir,wiki_event_tracer_privileges_file), 'w') as f:
+		f.write(header)
+		ol_keys = sorted(outputline.keys())
+		for olk in ol_keys:
+#			print ("olk: ",olk," \t| groupkey-olk: ",groupkey[olk]," \t | entity_names-g-o: ",entity_names[groupkey[olk]])
+			if entity_names[groupkey[olk]] != previous_key:
+				entity_name_fix_case = entity_names[groupkey[olk]]
+				entity_name_fix_case_end = entity_name_fix_case[1:].lower()
+				entity_name_fix_case_start = entity_name_fix_case[:1]
+				entity_name_fix_case = entity_name_fix_case_start + entity_name_fix_case_end
+				groupheader	= "|| h6. " + entity_name_fix_case + " || || || || || \n"
+				if entity_names[groupkey[olk]] in entities:				
+					f.write(groupheader)
+				previous_key = entity_names[groupkey[olk]]
+			if entity_names[groupkey[olk]] in entities:				
+				f.write(outputline[olk])
+
+
+
+
+
+        
+  
+# Calls the main() function
+if __name__ == '__main__':
+	main()
+
