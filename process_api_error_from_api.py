6cfcc0103146767c9a7d235d5298424a985c4813
Mary-Jane Smyth <maryjane.smyth@abiquo.com>
2014-10-15 18:44:09 +0200

    Move status codes to top and clean up wiki markup

--- process_api_error_from_api.py
+++ process_api_error_from_api.py
@@ -34,6 +34,8 @@ class ApiErrorLine:
 		wiki_label = self.label
 		wiki_internal_message_id = self.internal_message_id.strip("\"")
 		wiki_message = self.message.strip("\"")
+		wiki_message = re.sub("\|","\\\|",wiki_message)
+		wiki_message = re.sub("-","\\\-",wiki_message)
 	#	wiki_message = dowikimarkup(wiki_message)        
 	#  	print("| ",wiki.internal_message_id," | ",wiki.message," | ",wiki.label," |")
 		return '| %s | %s | %s | |' % (wiki_internal_message_id, wiki_message, wiki_label)
@@ -41,6 +43,8 @@ class ApiErrorLine:
 	def string_user(self):
 		wiki_internal_message_id = self.internal_message_id.strip("\"")
 		wiki_message = self.message.strip("\"")
+		wiki_message = re.sub("\|","\\\|",wiki_message)
+		wiki_message = re.sub("-","\\\-",wiki_message)
 	#	wiki_message = dowikimarkup(wiki_message)        
 	# 	print("| ",wiki.internal_message_id," | ",wiki.message," |")
 		return '| %s | %s | |' % (wiki_internal_message_id, wiki_message)
@@ -92,16 +96,33 @@ def main():
 #				print "section_data[skey]: " + section_data[skey] + " skey: " + skey + " ae_id: " + ae_id
 
 
-	for ss in sorted(error_lines):
+
+	error_key_list = list(error_lines.keys()) 
+#	print error_key_list
+#	Fiddle with the list to put the status code at the top of the list	
+	if "Status code" in error_key_list:
+		error_key_list.remove("Status code")
+	error_key_list = sorted(error_key_list)
+	error_key_list.insert(0,"Status code")	
+
+#	print error_key_list		
+
+#	for x in error_key_list:
+		# if re.match(x, "Status code"):
+		# 	print "i found it!"
+		# 	break
+		# else:
+		# 	x = None
+	for ee in error_key_list:
 #		print "ss: " + ss
 		# ab = re.sub("\D","",el)
 		# print ab
 #		for hh in error_lines[ss]:
 #			hi = re.sub("\D","",hh.internal_message_id)
 #			print "hi: " + hi
-		ss_interest = sorted(error_lines[ss], key=lambda XX: int("0" + (re.sub("\D","",XX.internal_message_id))))
+		ss_interest = sorted(error_lines[ee], key=lambda XX: int("0" + (re.sub("\D","",XX.internal_message_id))))
 #		ss_interest = sorted(error_lines[ss], key=sort_api_errors(error_lines[ss].internal_message_id))
-		print "|| h6. " + ss + " ||  ||  || ||"
+		print "|| h6. " + ee + " ||  ||  || ||"
 
 		for ael in ss_interest:
 			print ael.string_admin()
@@ -110,7 +131,6 @@ def main():
 
 #		ae_admin = aeline.string_admin()
 #		ae_user = aeline.string_user()
-
 
 
 
