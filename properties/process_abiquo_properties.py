0e47513747c79e17c901a66c202b0e10c81a391b
Mary-Jane Smyth <maryjane.smyth@gmail.com>
2015-02-20 13:48:29 +0100

    process_abiquo_properties.py

--- properties/process_abiquo_properties.py
+++ properties/process_abiquo_properties.py
@@ -1,31 +1,78 @@
 #!/usr/bin/python2 -tt
+import sys
+import json
 import re
+import collections
+
+class filedetails:
+	def __init__(self,aprefix,asuffix,adate,aiprefix,aisuffix):
+		self.fprefix = aprefix
+		self.fsuffix = asuffix
+		self.fdate = adate
+		self.iprefix = aiprefix
+		self.isuffix = aisuffix
 
 class prop:
-	def __init__(self,apropName,apropCategory,apropType,apropDescription,apropDefault,apropRange):
+	def __init__(self,apropName,apropCategories,apropType,apropDescription,apropDefault,apropRange):
 		self.pName = apropName
-		self.pCategory = apropCategory
-		self.pType = apropType
-		self.pDescription = apropDescription
 		self.pDefault = apropDefault
+		self.pDescription = apropDescription
 		self.pRange = apropRange
+		self.pCategories = apropCategories
+		self.pType = apropType
+		
 	
 	def pprint(self):
-		prCategory = " ".join(self.pCategory)
-		print "| %s | %s | %s | %s | %s | %s |" % (self.pName,prCategory,self.pType,self.pDescription,self.pDefault,self.pRange)
+		prCategories = " ".join(self.pCategories)
+		print "| %s | %s | %s | %s | %s | %s |" % (self.pName,self.pDefault,self.pRange,self.pDescription,prCategories,self.pType)
 
-class main():
+
+def tree(): return collections.defaultdict(tree)
+
+def wikiProperty(rawProp,profiles,filedetails):
+	property_entry['propertyName'] = rawProp.pName
+	property_entry['propertyDefault'] = rawProp.pDefault
+	property_entry['propertyRange'] = rawProp.pRange
+	property_entry['propertyDescription'] = rawProp.pDescription
+	property_entry['propertyProfiles'] = []
+
+	fileName = filePrefix + "_"  + fileDate + "." + fileSuffix
+
+	for profile in profiles:
+		if profile in prCategories:
+			property_profile_item = {}
+			property_profile_item['profileExampleFile'] = filedetails.fprefix + filedetails.fdate + profile.lower() + filedetails.fsuffix
+			property_profile_item['profileExampleImage'] = filedetails.iprefix + profile.lower + filedetails.isuffix
+		else:
+			property_profile_item = {}
+			property_profile_item['profileExampleFile'] = ""
+			property_profile_item['profileExampleImage'] = ""
+		property_entry['propertyProfiles'].append(property_profile_item)	
+
+
+def main():
 	# Read git properties file line by line
 	with open("abiquo.properties.txt") as f:
 		content = f.readlines()
 	
+	fileDate = "_2015-03-31"
+	filePrefix = "properties_"
+	fileSuffix = ".txt"
+	imagePrefix = "v26_symbol_"
+	imageSuffix = "_transparent.png"
+	fdetails = filedetails(fileDate,filePrefix,fileSuffix,imagePrefix,imageSuffix)
+
 	property_description_list = []
+	
 	property_name = ""
-	property_category = []
+	property_categories = []
 	property_description = ""
 	property_type = ""
 	property_default = ""
 	property_range = ""
+	property_dict = tree()
+
+	profiles = {"API": "SERVER","RS": "REMOTESERVICE","V2V": "V2VSERVICES","OA":"M OUTBOUND API"}
 
 	for property_line in content:
 		if re.match("\n", property_line):
@@ -42,17 +89,12 @@ class main():
 #			print  property_line
 			if re.match("#####",property_line):
 #				print "matched profiles"
-				property_category = []
+				property_categories = []
 				if re.search("MULTIPLE PROFILES",property_line):
 					continue
-				if re.search("SERVER",property_line):
-					property_category.append("SERVER")
-				if re.search("REMOTESERVICE",property_line):
-					property_category.append("REMOTESERVICE")
-				if re.search("V2VSERVICES",property_line):
-					property_category.append("V2VSERVICES")
-				if re.search("M OUTBOUND API",property_line):
-					property_category.append("OUTBOUNDAPI")
+				for profile in profiles:
+					if re.search(profiles[profile],property_line):
+						property_categories.append(profile)
 			else:
 #				print "no matched comment"
 #				print property_line
@@ -95,7 +137,8 @@ class main():
 			if property_range_search:
 				property_range = property_range_search.group(2) 
 				property_description = re.sub(property_range_search.group(0),"",property_description)
-			aproperty = prop(property_name,property_category,property_type,property_description,property_default,property_range)
+			aproperty = prop(property_name,property_categories,property_type,property_description,property_default,property_range)
+
 			aproperty.pprint()
 	# Use mustache
 	# Create a table like in the wiki
