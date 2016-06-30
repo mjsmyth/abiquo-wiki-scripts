0638bb45a6c7b20ec649f3ece0f06258c3384e39
Mary-Jane Smyth <maryjane.smyth@abiquo.com>
2016-06-30 17:53:52 +0200

    basic version working

--- releasenotes/process_rel_notes.py
+++ releasenotes/process_rel_notes.py
@@ -6,6 +6,7 @@
 ## 
 import os
 import re
+import collections
 
 class Issue:
 	def __init__(self,a_issueNo,a_issueType,a_issueDesc):
@@ -13,11 +14,26 @@ class Issue:
 		self.issueType=a_issueType
 		self.issueDesc=a_issueDesc
 
-	def wiki_issue_string(self):
+	def wikiIssueString(self):
 		wikiIssueNo = str(self.issueNo)
 		wikiIssueType = self.issueType
 		wikiIssueDesc = doWikiMarkup(self.issueDesc)        
 		return (wikiIssueType,'| %s | %s | | \n' % (wikiIssueNo, wikiIssueDesc))
+
+	def __repr__(self):
+		return repr((self.issueNo, self.issueType, self.issueDesc))	
+
+def doWikiMarkup(my_wiki_message):
+	a_wiki_message = my_wiki_message.replace(r"\\",r"\\\\")
+	a_wiki_message = re.sub("\|","\\\|",a_wiki_message)
+	a_wiki_message = re.sub("-","\\\-",a_wiki_message)
+#	a_wiki_message = a_wiki_message.strip("\"")
+#	a_wiki_message = re.sub(r"\\\\\|",r"\\\|",a_wiki_message)
+	a_wiki_message = a_wiki_message.replace("{",r"\{")
+	a_wiki_message = a_wiki_message.replace("}",r"\}")
+	a_wiki_message = a_wiki_message.replace("[",r"\[")		
+	a_wiki_message = a_wiki_message.replace("]",r"\]")
+	return a_wiki_message			
 
 def main():
 	inputSubdir = "input_files"
@@ -30,35 +46,125 @@ def main():
 # TODO get from Confluence
 	relNotesInputFile = "rel_notes_draft_" + currentVersion + ".txt"
 	FS = "|"
-	improvLines = {}
-	improvSorted = {}
-	issueLines = {}
-	issueSorted = {}
+	previousStore = {}
+	currentStore = {}
+	editedStore = {}
+	revisionList = []
+
 
 	relNotesOutputFile = "rel_notes_wiki_" + currentVersion + ".txt"
 
-	relNotesEditedFile = "rel_notes_wiki_" + previousVersion + ".txt"
 	relNotesStorageFile = "rel_notes_wiki_storage_format_" + previousVersion + ".txt"
 
 	relNotesHeader = "|| Key || Description ||   ||\n"; 
 
 
-	relNotesInputList = [reln.strip() for reln in open(os.path.join(inputSubdir,relNotesStorageFile))]
-	currentIssueType = ""
-	for relNoteItem in relNotesInputList:		
+	relNotesPreviousList = [reln.strip() for reln in open(os.path.join(inputSubdir,relNotesStorageFile))]
+	previousIssueType = ""
+	for relNoteItemPrevious in relNotesPreviousList:		
 # check it is a wiki line
 # in basic case, it should have some pipe characters
-		relNote = re.split('(?<!=)\|',relNoteItem)
-		print "%d" % (len(relNote))
-		if len(relNote) != 4:
-			relNoteString = " ".join(relNote)
-			if re.search("Planned Improvements",relNoteString):
+		relNotePrevious = re.split('(?<!=)\|',relNoteItemPrevious)
+		if len(relNotePrevious) != 4:
+			relNotePreviousString = " ".join(relNotePrevious)
+			if re.search("Planned Improvements",relNotePreviousString):
+				previousIssueType = "Improvement"
+			if re.search("Known Issues",relNotePreviousString):
+				previousIssueType = "Issue"				
+		else:
+#			print "Previous | %s | %s | %s |" % (previousIssueType, str(relNotePrevious[1].strip()),relNotePrevious[2].strip())
+			previousIssue = Issue(str(relNotePrevious[1].strip()),previousIssueType,relNotePrevious[2].strip())
+			previousStore[str(relNotePrevious[1].strip())] = previousIssue
+
+	relNotesCurrentList = [reln.strip() for reln in open(os.path.join(inputSubdir,relNotesInputFile))]
+	currentIssueType = ""
+	for relNoteItemCurrent in relNotesCurrentList:		
+# check it is a wiki line
+# in basic case, it should have some pipe characters
+		relNoteCurrent = re.split('(?<!=)\|',relNoteItemCurrent)
+		if len(relNoteCurrent) != 4:
+			relNoteCurrentString = " ".join(relNoteCurrent)
+			if re.search("Improvement",relNoteCurrentString):
 				currentIssueType = "Improvement"
-			if re.search("Known Issues",relNoteString):
+			if re.search("Bug",relNoteCurrentString):
 				currentIssueType = "Issue"				
 		else:
-			print "relNote: %s %s %s" % (currentIssueType, str(relNote[1]),str(relNote[2]))
+#			print "Current | %s | %s | %s |" % (currentIssueType, str(relNoteCurrent[1].strip()),relNoteCurrent[2].strip())
+			currentIssue = Issue(str(relNoteCurrent[1].strip()),currentIssueType,relNoteCurrent[2].strip())
+			currentStore[str(relNoteCurrent[1]).strip()] = currentIssue
 
+	validSelection = {"c","p"}
+
+	for currentKey in currentStore:
+		reviseSelection = ""
+		mySelection = ""		
+		print "%s : %s - %s" % (currentStore[currentKey].issueType, currentKey, currentStore[currentKey].issueDesc)
+		if currentKey in previousStore:
+			print "c: %s" %  currentStore[currentKey].issueDesc	
+			print "p: %s" %  previousStore[currentKey].issueDesc
+			while mySelection not in validSelection:
+				mySelection = raw_input("p = previous, c = current: ").strip()
+			else:	
+				editedStore[currentKey] = currentStore[currentKey]
+				if re.match("p",mySelection):
+					editedStore[currentKey].issueDesc = previousStore[currentKey].issueDesc
+				# elif re.match("e",mySelection):
+				# 	newDescription = raw_input("Enter a new description:").strip()
+				# 	confirmNewDesc = raw_input("Enter 'y' to save new description': %s" % newDescription).strip()
+				# 	if confirmNewDesc == "y":
+				# 		editedStore[currentKey].issueDesc = newDescription		
+				# if re.search("r",mySelection):
+				# 	revisionList.append(currentKey)
+		else:
+			editedStore[currentKey] = currentStore[currentKey]
+
+			# mySelection = ""
+			# newValidSelection = {"c","cr"}
+			# print "c: %s" % currentStore[currentKey].issueDesc
+			# while mySelection not in newValidSelection: 
+			# 	mySelection = raw_input("c = current, cr=+revise: ").strip()
+			# else:	
+			# 	editedStore[currentKey] = currentStore[currentKey]		
+			# 	# if re.match("e",mySelection):
+			# 	# 	newDescription = raw_input("Enter a new description:").strip()
+			# 	# 	confirmNewDesc = raw_input("Enter 'y' to save new description': %s" % newDescription).strip()
+			# 	# 	if confirmNewDesc == "y":
+			# 	# 		editedStore[currentKey].issueDesc = newDescription					
+			# 	if re.search("r",mySelection):
+			# 		revisionList.append(currentKey)
+
+	outputStore = collections.OrderedDict(sorted(editedStore.iteritems(), key=lambda t: t[1]))			
+
+	outfileWiki = open(os.path.join(outputSubdir,relNotesOutputFile), 'w')
+
+	wikiHeader = "|| Key || Summary || ||"
+
+	headerImprovementPrinted = False
+	headerIssuePrinted = False
+
+	for outputKey in outputStore:
+	#	print "| %s | %s | %s |" % (outputStore[outputKey].issueType, outputStore[outputKey].issueNo,outputStore[outputKey].issueDesc)					
+	# for revisionItem in revisionList:
+	# 	print "Revise: %s" % revisionItem
+
+		(currentIssue,outWikiString) = outputStore[outputKey].wikiIssueString()
+	
+		if currentIssue == "Improvement" and headerImprovementPrinted == False:
+			currentIssueHeader = "h4. %s \n" % currentIssue
+			outfileWiki.write (currentIssueHeader)
+
+			print ("|| Key || Summary || ||") 
+			headerImprovementPrinted = True
+
+		if currentIssue == "Issue" and headerIssuePrinted == False:
+			currentIssueHeader = "h4. %s \n" % currentIssue
+			outfileWiki.write (currentIssueHeader)
+
+			print "h4. %s \n" % currentIssue
+			headerIssuePrinted = True
+
+		outfileWiki.write (outWikiString)	
+		print "%s" % outWikiString		
 
 # Calls the main() function
 if __name__ == '__main__':
