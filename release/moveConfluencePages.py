d243c2c56b5945af8c6e5ccc485c761fcb94f3d7
Mary-Jane Smyth <maryjane.smyth@abiquo.com>
2019-09-19 13:15:25 +0200

    add some stuff and spaces

--- release/moveConfluencePages.py
+++ release/moveConfluencePages.py
@@ -1,7 +1,7 @@
 # Python script: moveConfluencePages
 # ---------------------------------------
-# Script designed to move draft pages from the main space to a sub page
-# After the release is complete
+# Script designed to move draft pages from the main space to under another page
+# Use after the release is complete
 #
 import requests
 import os
@@ -16,7 +16,7 @@ from requests.auth import HTTPBasicAuth
 #2. Get the content ID and version of the page
 #3. Add 1 to the version number
 #4. Add "ancestors":[{"id":47535934}]
-#5. Rename the page (check if failed string replace does not rename)
+#5. "Rename" the page with fake foo bar replace :-D
 # curl -X PUT -H "Authorization: ..." -H "Content-Type: application/json" -d 
 #              '{"id":1234567,
 #                  "type":"page", 
@@ -25,19 +25,23 @@ from requests.auth import HTTPBasicAuth
 #                   "space":{"key":"xxx"},
 #                   "version":{"number":17}}' 
 # "https://confluence.ges.symantec.com/rest/api/content/1234567"
-
-# just get this from the URL under page information :-D
+#
+# This is stable, so just get this from the URL when displaying Page information :-D
+# testing page = PAGESPARK
 # move_to_page_id = "47535936"
+# testing page = Completed pages
 move_to_page_id = 47526929
 
 # Get user credentials and space
-site_URL = input("Confluence site URL (exclude protocol & final slash): ")
+site_URL = input("Enter Confluence site URL (exclude protocol & final slash): ")
 username = input("Username: ")
 pwd = input("Password: ")
 spacekey = input("Space key: ")
+to_spacekey = spacekey[:]
+release_version = input("Release version, e.g. v463: ")
 
+# API and Authentication
 apiUrl = 'https://' + site_URL
-
 apiHeaders = {}
 apijson = 'application/json'
 apiHeaders['Accept'] = apijson
@@ -54,8 +58,14 @@ apiHeadersPut = {}
 apiHeadersPut['Content-Type'] = apijson[:]
 apiHeadersPut['Accept'] = apijson[:]
 
-searchQueryCql ='?cql=siteSearch+~+"v463"+and+space+%3D+"COMP"&queryString=v463'
-#searchQueryCql = '?cql=space+%3D+\"COMP\"+and+title+~+\"v461\"'
+# Search for entities containing release_version string - can be attachments
+# Can also be pages with the release_version string in the body text
+# These will be el¡minated when the 
+searchQueryCql ='?cql=siteSearch+~+"' + release_version + '"+and+space+%3D+"' + spacekey + '"&queryString=' + release_version
+# Old search
+# searchQueryCql ='?cql=siteSearch+~+"v463"+and+space+%3D+"COMP"&queryString=v463'
+# Title search: when you enter v46, it gets all v461, v462, v463... 
+# searchQueryCql = '?cql=space+%3D+\"COMP\"+and+title+~+\"v461\"'
 
 searchUrl = apiUrl + '/rest/api/content/search' + searchQueryCql
 
@@ -69,55 +79,69 @@ while True:
     for page in results["results"]:
         page_id = str(page["id"])
         print ("hello: ",page_id)
-        # only move pages, not attachments
-        if "att" in page_id:
-            print("att: ",page_id)
-        else:   
-            page_name = page["title"]
 
-            # Do a fake replace instead of a rename to force a move. Hopefully no pages with foofoo in their name        
-            new_page_name = (str(page_name)).replace("foofoo","barbar")
-            page_uri = apiUrl + '/rest/api/content/' + page_id 
-            p = requests.get(page_uri, headers=apiHeaders, auth=HTTPBasicAuth(username,pwd), params=apiParams)
-            page_got = p.json()
+        # check the vXXX or whatever is in the page title and not only in the page content 
+        # note that the specific title search will get vXXX when you use vXX (e.g. v463 from v46)
+        if release_version.strip().lower() in (str(page["_links"]["webui"])).lower():
 
-            page_version = page_got["version"]["number"]
+            # only move pages, not attachments
+            if "att" in page_id:
+                print ("Page is an attachment: ",page_id)
+            else:   
+                page_name = page["title"]
 
-            # apparently the list is ordered and this gets the last ancestor ???? 
-            current_ancestor_id = (page_got["ancestors"].pop())["id"]
-            print("current ancestor: ",current_ancestor_id)
+                # Do a fake replace instead of a rename to force a move. Hopefully no pages with foofoo in their name 
+                # Can always do a replace of barbar right :-D       
+                new_page_name = (str(page_name)).replace("foofoo","barbar")
+                page_uri = apiUrl + '/rest/api/content/' + page_id 
 
-            if current_ancestor_id == str(move_to_page_id):
-                print ("Page already there: ",page_id)
-            else:
-        #        print (str(page_version))
-        #        page_name_qualified = (re.sub(r'([^\s\w]|_)+', '', page_name)) + "-" + page_id
-                pageUrlForList = "**" + apiUrl + page["_links"]["webui"] + "**  ID: "+ page_id + " * version: " + str(page_version)
-        #        pageUrlForList = "**" + apiUrl + page["_links"]["webui"] + "** \n"
-                print (pageUrlForList)
-        #        page_put = copy.deepcopy(page_got)
-                page_put = {}
-                page_put["id"] = page_id
-                page_put["type"] = page_got["type"]
-                page_put["title"] = new_page_name
-                page_put["status"] = "current"
-        #       page_put["body"] = copy.deepcopy(page_got["body"])
-        #       page_put["ancestors"] = [{"id":47535934}]
-                page_put["ancestors"] = [{"id": move_to_page_id}]
-                page_put["space"] = {}
-                page_put["space"]["key"] = spacekey
-                page_put["version"] = {}
-                page_put["version"]["number"] = page_version + 1
-                print ("page_put: ", str(page_put))
-                page_put_serialized = json.dumps(page_put)
-        #        print ("params: ",apiParams,"\n")
-        #        print ("headers: ",str(apiHeadersPut),"\n")
-                print ("page uri: ",page_uri)
-                try:    
-                    q = requests.put(page_uri, headers=apiHeadersPut, auth=HTTPBasicAuth(username,pwd), data=page_put_serialized, params=apiParams)
-                    print (str(q),"\n")
-                except:
-                    print (r.status_code)    
+                # Get more page details with expands
+                p = requests.get(page_uri, headers=apiHeaders, auth=HTTPBasicAuth(username,pwd), params=apiParams)
+                page_got = p.json()
+                print (str(page_got))
+
+                page_version = page_got["version"]["number"]
+                                
+                # apparently the list is ordered and this gets the last ancestor ???? 
+                if page_got["ancestors"]:
+                    current_ancestor_id = (page_got["ancestors"].pop())["id"]
+                    print("current ancestor: ",current_ancestor_id)
+
+                    if current_ancestor_id == str(move_to_page_id):
+                        print ("Page already there: ",page_id)
+                    else:
+                #        print (str(page_version))
+                #        page_name_qualified = (re.sub(r'([^\s\w]|_)+', '', page_name)) + "-" + page_id
+                        pageUrlForList = "**" + apiUrl + page["_links"]["webui"] + "**  ID: "+ page_id + " * version: " + str(page_version)
+                #        pageUrlForList = "**" + apiUrl + page["_links"]["webui"] + "** \n"
+                        print (pageUrlForList)
+                #        page_put = copy.deepcopy(page_got)
+                        page_put = {}
+                        page_put["id"] = page_id
+                        page_put["type"] = page_got["type"]
+                        page_put["title"] = new_page_name
+                        page_put["status"] = "current"
+                #       page_put["body"] = copy.deepcopy(page_got["body"])
+                #       page_put["ancestors"] = [{"id":47535934}]
+                        page_put["ancestors"] = [{"id": move_to_page_id}]
+                        page_put["space"] = {}
+                        page_put["space"]["key"] = to_spacekey
+                        page_put["version"] = {}
+                        page_put["version"]["number"] = page_version + 1
+                        print ("page_put: ", str(page_put))
+                        page_put_serialized = json.dumps(page_put)
+                #        print ("params: ",apiParams,"\n")
+                #        print ("headers: ",str(apiHeadersPut),"\n")
+                        print ("page uri: ",page_uri)
+                        try:    
+                            q = requests.put(page_uri, headers=apiHeadersPut, auth=HTTPBasicAuth(username,pwd), data=page_put_serialized, params=apiParams)
+                            print (str(q),"\n")
+                        except:
+                            print (r.status_code)    
+                else:
+                    print ("Page has no ancestors: ",page_id)
+        else:
+            print ("Page does not have ",release_version," in name: ",(str(page["_links"]["webui"])).lower())                
     if "next" not in results["_links"]:
         break
     else:
