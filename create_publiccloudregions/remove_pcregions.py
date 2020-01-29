d8333508468454943de7056179bd77c98f77372c
Lynda Mary-Jane Smyth <maryjane.smyth@gmail.com>
2020-01-29 23:05:26 +0100

    Change --s to API URL

--- create_publiccloudregions/remove_pcregions.py
+++ create_publiccloudregions/remove_pcregions.py
@@ -5,7 +5,7 @@
 # ============
 # Environment: With access to internet
 # Removes a page of PCRs in order returned by the API
-#
+# Can't remove PCRs with stuff in them
 
 from abiquo.client import Abiquo
 import argparse
@@ -13,39 +13,32 @@ import argparse
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-LOCALDOMAINAPI = ".bcn.abiquo.com/api"
-PROVIDERCODES = {"AMAZON": "AWS", "azurecompute-arm": "AZ"}
-PROVIDERSLIST = ["AMAZON", "azurecompute-arm"]
-PCRREMOTESERVICES = ["NARS", "VIRTUALSYSTEMMONITOR",
-                     "VIRTUALFACTORY", "REMOTEACCESS"]
-REMOTESERVICESID = "mjsabiquo"
-
 
 def main():
 
-    parser = argparse.ArgumentParser(description='Create PCRs!')
-    parser.add_argument("--s", default="mjsabiquo",
-                        type=str, help="Local system")
+    parser = argparse.ArgumentParser(description='Remove a page of PCRs!')
+    parser.add_argument("--s", default="https://mjsabiquo.bcn.abiquo.com/api",
+                        type=str, help="API url")
     parser.add_argument("--u", default="admin",
                         type=str, help="Username")
     parser.add_argument("--p", default="xabiquo",
                         type=str, help="Password")
 
     args = parser.parse_args()
-    localsystem = args.s
+    apiUrl = args.s
     username = args.u
     password = args.p
 
-    API_URL = "https://" + localsystem + LOCALDOMAINAPI
-    api = Abiquo(API_URL, auth=(username, password), verify=False)
+    api = Abiquo(apiUrl, auth=(username, password), verify=False)
 
     code, pcrsList = api.admin.publiccloudregions.get(
         headers={'Accept': 'application/vnd.abiquo.publiccloudregions+json'})
     print("Get public cloud regions. Response code is: ", code)
 
     for pcr in pcrsList:
-        code = pcr.follow('edit').delete()
-        print("Delete public cloud region. Response code is: ", code)
+        print("Delete public cloud region: ", pcr.name)
+        code, pcrobject = pcr.follow('edit').delete()
+        print("Response code is: ", code)
 
 
 # Calls the main() function
