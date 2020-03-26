abc574e45aba7744b34b0a6a9c6fe15d67bff247
System Administrator <maryjane.smyth@gmail.com>
2020-03-26 18:01:03 +0100

    Add a trim to make it work for files that weren't working

--- screenshot_processing/process_screenshots.sh
+++ screenshot_processing/process_screenshots.sh
@@ -1,48 +1,54 @@
 #!/bin/zsh
 
 #file_list=`ls -1d /Users/maryjane/Pictures/screenshots/v50 | grep "v50_" | grep "png" | grep -v "d_v50" | awk '{print $0}'`
+directory="/Users/maryjane/Pictures/screenshots/v50/"
 
-file_list=`ls -1 /Users/maryjane/Pictures/screenshots/v50/ | grep "v50_" | grep "png" | grep -v "dv50" | grep -v "d_v50" | awk '{print $0}'`
+file_list=`ls -1 ${directory} | grep "v50_" | grep "png" | grep -v "dv50" | grep -v "d_v50" | awk '{print $0}'`
 
 filearray=($file_list)
 
 if [[ `echo ${filearray[*]}` == "" ]]; then
-         print "files not received"
+         print "Files not received"
 else
  for ifile in `echo ${filearray[*]}`; do
 
-        width=$(magick identify "$ifile" | awk -F "[ ]\|x" '{print $3}')
-        height=$(magick identify "$ifile" | awk -F "[ ]\|x" '{print $4}')
+        width=$(magick identify "${directory}$ifile" | awk -F "[ ]\|x" '{print $3}')
+        height=$(magick identify "${directory}$ifile" | awk -F "[ ]\|x" '{print $4}')
 
         print "$ifile: ${width}x${height}"
 
         ifileout=$(print "d_${ifile}")   
 #        print "$ifileout"
 
-        convert "${ifile}" \
+        convert "${directory}${ifile}" \
          -bordercolor gray75 -compose copy -border 3 \
          -bordercolor gray58 -compose copy -border 3 \
-         -fuzz 50% -trim +repage "${ifileout}" 
+         -fuzz 50% -trim +repage "${directory}${ifileout}" 
 
-        new_width=$(magick identify "$ifileout" | awk -F "[ ]\|x" '{print $3}')
-        new_height=$(magick identify "$ifileout" | awk -F "[ ]\|x" '{print $4}')
+        new_width=$(magick identify "${directory}$ifileout" | awk -F "[ ]\|x" '{print $3}')
+        new_height=$(magick identify "${directory}$ifileout" | awk -F "[ ]\|x" '{print $4}')
 
         change_width=$(( width - new_width ))
         print "changewidth: $change_width"
 
-        if [[ change_width -ge 200 ]]; then
-            cp $ifile $ifileout
+        if [[ change_width -ge 20 ]]; then
+            cp ${directory}$ifile ${directory}$ifileout
         else
-            convert "${ifile}" \
-            -bordercolor gray75 -compose copy -border 3 \
-            -bordercolor gray58 -compose copy -border 3 \
-            -fuzz 50% -trim +repage \
-            -fuzz 50% -trim +repage "${ifileout}" 
-            
+            if [[ change_width -eq 1 ]]; then
+                convert "$directory${ifile}" -crop "${new_width}x${new_height}+0+0" +repage \
+                -bordercolor gray75 -compose copy -border 3 \
+                -bordercolor gray58 -compose copy -border 3 \
+                -fuzz 50% -trim +repage \
+                -fuzz 50% -trim +repage "$directory${ifileout}" 
+            else    
+                convert "${directory}${ifile}" \
+                -bordercolor gray75 -compose copy -border 3 \
+                -bordercolor gray58 -compose copy -border 3 \
+                -fuzz 50% -trim +repage \
+                -fuzz 50% -trim +repage "${directory}${ifileout}" 
+            fi
         fi    
 
         print "Processed file ${ifileout}" 
  done
 fi
-
-#         
