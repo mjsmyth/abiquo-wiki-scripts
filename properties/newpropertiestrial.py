17d5b550de52e488beed97c85ade00173c28e0e6
Lynda Mary-Jane Smyth <maryjane.smyth@gmail.com>
2021-01-26 18:12:32 +0100

    first part of trial version

--- properties/newpropertiestrial.py
+++ properties/newpropertiestrial.py
@@ -1,17 +1,50 @@
 #!/usr/bin/python3 -tt
 # New attempt at processing Abiquo properties
+# Hopefully this time it is developer proof
 #
 import codecs
 import re
 import json
-from collections import Counter
+# from collections import Counter
+import copy
 
 
-def groupLines(line, startComment, lineStore):
-    # is it a profile line
-    # is it a property description line
-    #########
-    return "hello"
+def processGroups(propName):
+    # METRICS = ["cpu", "cpu-mz", "cpu-time", "memory",
+    #            "memory-swap", "memory-swap2", "memory-vmmemctl",
+    #            "memory-physical", "memory-host", "disk-latency",
+    #            "uptime"]
+    BACKUPS = ["veeam95u4", "veeam10", "networker", "veeam"]
+    PLUGINS = ["amazon", "kvm", "vmx_04",
+               "rackspace-cloudservers-uk",
+               "softlayer", "esx", "vcenter_cluster",
+               "openstack-nova", "packet",
+               "hyperv_301", "oracle-ase-emea",
+               "azurecompute-arm", "oracle-ase-us",
+               "vcd", "xenserver",
+               "digitalocean2", "google-compute-engine",
+               "rackspace-cloudservers-us", "cloudoorsphere"]
+    NETWORKS = ["openstack-neutron", "dnsmasq", "omapi",
+                "nsx-ecmp", "logical",
+                "nsx-nat", "nsx-gateway"]
+    groupTypes = {"{backupPlugin}": BACKUPS,
+                  "{computePlugin}": PLUGINS,
+                  "{devicePlugin}": NETWORKS}
+
+    propNameList = propName.split(".")
+    # plugins etc are not in first two parts of name
+    # filter list of plugins in each list
+    lastPropNameList = propNameList[2:]
+
+    for group, groupList in groupTypes.items():
+        groupTagList = [x for x in lastPropNameList if x in groupList]
+        if groupTagList:
+            # print("propName devices: ", propName, devicesGroup)
+            groupTag = groupTagList[0]
+            groupPattern = propName.replace(groupTag, "XXX")
+            groupName = propName.replace(groupTag, group)
+            return (groupTag, groupPattern, groupName)
+    return("", "", "")
 
 
 def main():
@@ -23,29 +56,15 @@ def main():
     propertyFile = 'abiquo.properties'
     dirPropertyFile = inputDir + propertyFile
 
-    STUPIDLIST = ["#device plugins", "#backup plugins"]
-    METRICS = ["cpu", "cpu-mz", "cpu-time", "memory",
-               "memory-swap", "memory-swap2", "memory-vmmemctl",
-               "memory-physical", "memory-host", "disk-latency",
-               "uptime"]
-    PLUGINS = ["amazon", "kvm", "vmx_04",
-               "rackspace-cloudservers-uk",
-               "softlayer", "esx", "vcenter_cluster",
-               "openstack-nova", "packet",
-               "hyperv_301", "oracle-ase-emea",
-               "azurecompute-arm", "oracle-ase-us",
-               "vcd", "xenserver",
-               "digitalocean2", "google-compute-engine",
-               "rackspace-cloudservers-us", "cloudoorsphere",
-               "openstack-neutron", "nsx-gateway",
-               "nsx-nat", "logical", "nsx-ecmp",
-               "omapi", "dnsmasq",
-               "veeam95u4", "veeam10", "networker", "veeam"]
-    DEVICES = ["openstack-neutron", "dnsmasq", "omapi",
-               "nsx-ecmp", "logical",
-               "nsx-nat", "nsx-gateway"]
+    # NARSPROPERTY = r"#abiquo\.nars\.async\.pool"
+    NARSCOMMENT = ("Maximum number of simultaneous operations on a single "
+                   "hypervisor or region connection, by type. "
+                   "Default abiquo.nars.async.pool.max")
 
-    propertiesList = []
+    NARSLIST = ["device plugins", "backup plugins"]
+
+    groupDict = {}
+    propertiesDict = {}
     groupStore = []
     propertyDict = {}
     profiles = []
@@ -80,7 +99,7 @@ def main():
             propertyList = []
             comment = ""
             groupLines = group.split("\n")
-            # find all the properties in a group
+            # find all the properties in a group that may include comments
             pValue = re.finditer(propertySearchString, group)
             if pValue:
                 for pv in pValue:
@@ -89,12 +108,10 @@ def main():
                     # add each property to the corresponding propfiles
                     for profile in profiles:
                         propertyDict[profile].append(group)
-            # get comments without whitespace between them
+            # get comments without whitespace between them and their properties
             for groupLine in groupLines:
                 if groupLine[:] not in propertyList:
                     comment += groupLine.strip("#")
-                    if groupLine[:] in STUPIDLIST:
-                        comment = commentStore[-1][-1]
                 else:
                     commentStore.append([groupLine, comment])
                     comment = ""
@@ -115,28 +132,31 @@ def main():
             propName = currentProp
             propDefault = ""
         propName = propName.replace("#", "")
-        propNameList = propName.split(".")
-        # plugins etc are not in first two parts of name
-        # filter list of plugins in each list
-        lastPropNameList = propNameList[2:]
-        metricsGroup = [x for x in lastPropNameList if x in METRICS]
-        if metricsGroup:
-            print("propName metrics: ", propName, metricsGroup)
 
-        devicesGroup = [x for x in lastPropNameList if x in DEVICES]
-        if devicesGroup:
-            print("propName devices: ", propName, devicesGroup)
+        # # Metrics group is not in the file :-p
+        # metricsGroup = [x for x in lastPropNameList if x in METRICS]
+        # if metricsGroup:
+        #     print("propName metrics: ", propName, metricsGroup)
+        groupTag, groupPattern, groupName = processGroups(propName)
 
-        pluginsGroup = [x for x in lastPropNameList if x in PLUGINS]
-        if pluginsGroup:
-            print("propName devices: ", propName, pluginsGroup)
+        if groupPattern:
+            if groupPattern not in groupDict:
+                groupDict[groupPattern] = {}
+            if groupName not in groupDict[groupPattern]:
+                groupDict[groupPattern][groupName] = []
+            groupDict[groupPattern][groupName].append(propName)
 
-        propDict[propName] = [propName, propDefault]
+        propDict[propName] = {}
+        propDict[propName]["name"] = copy.deepcopy(propName)
+        propDict[propName]["default"] = copy.deepcopy(propDefault)
 
         if line[1]:
             propRange = ""
             propComment = line[1]
             propComment = re.sub(r"^\s?", "", propComment)
+            # deal with the case of interleaving comments
+            if propComment in NARSLIST:
+                propComment = NARSCOMMENT
             propRangeFound = re.search(
                 r"(Range[\s]*?:[\s]*?)(.*)", propComment)
             if propRangeFound:
@@ -144,62 +164,30 @@ def main():
                 propRange = propRangeFound.group(2)
                 propComment = re.sub(propRangeString, "", propComment)
 
-        propDict[propName].append(propComment)
-        propDict[propName].append(propRange)
-    propertiesList.append(propDict)
+        propDict[propName]["comment"] = copy.deepcopy(propComment)
+        propDict[propName]["range"] = copy.deepcopy(propRange)
 
-#    for props in propertiesList:
-#        print(json.dumps(props, indent=2))
+        propertiesDict[propName] = copy.deepcopy(propDict)
 
+    # A property group has two or more items
+    count = 0
+    groupToRemove = []
+    for groupPattern, groupNameDict in groupDict.items():
+        for g, names in groupNameDict.items():
+            count += len(names)
+        if count < 2:
+            groupToRemove.append(groupPattern)
+        count = 0
+    # Remove invalid groups
+    for groupToRemoveName in groupToRemove:
+        groupDict.pop(groupToRemoveName)
 
-        # numberOfNameOccurrences = Counter(
-        #     propNameOccurs.split(" "))
-        # propDict[propName].append(numberOfNameOccurrences)
+    print(json.dumps(groupDict, indent=2))
 
-                    # propertyAndDefault = pv[0]
-                    # print("propertyAndDefault: ", propertyAndDefault)
-                    # propertyName = pv[1]
-                    # print("name: ", propertyName)
-                    # propertyDefault = pv[-1]
-                    # print("default: ", propertyDefault)
-
-
-            #     propertyGroup.append(comment)
-            #     propertiesWithComments.append(propertyGroup)
-            # for propertyGroup in propertiesWithComments:
-            #     if len(propertyGroup) > 2:
-            #         numberOfNameOccurrences = Counter(
-            #             nameElementsString.split(" "))
-            #         print ("num of name occ: ",
-            #                json.dumps(numberOfNameOccurrences, indent=2))
-#                for name, num in numberOfNameOccurrences.items():
-#                    if num > 1:
-#                        baseName.append(name)
-#                    else:
-#                        variables.append(name)
-                # print("baseName: ", baseName)
-                # print("variables: ", variables)
-
-            
-            # print (propertyValues)
-            # print (otherPropertyText)
-            # for pv in propertyValues:
-            #     print (pv)
-            #     if pv[0]:
-            #         propertySection = pv[0]
-            #         print("section: ", propertySection)
-            #         propertyName = pv[1]
-            #         print("name: ", propertyName)
-            #         defaultValue = pv[-1]
-            #         print("default: ", defaultValue)
-            # print("group: ", group)
-
-
-    # print(json.dumps(propertyDict, indent=2))
-    # Discard the comments at start of file
-    # for group in groupStore:
-    #    print (group)
-    #    print ("---")
+    # TO DO:
+    # - Add valid group names to properties
+    # - Add profiles to properties
+    # - Write properties to a file
 
 
 # Calls the main() function
